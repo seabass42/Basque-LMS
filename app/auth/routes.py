57d2654e94548f10689eed3785c37af1bd0496f9
ann7vip-coder <alan.ho@sjsu.edu>
2025-12-08 18:53:22 -0800

    Refactor login route to use form handling
    Refactor login route to use LoginForm and separate login logic.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,31 +1,27 @@
 from flask import render_template, Blueprint, redirect, url_for, flash, request
-from app.models import User
-from werkzeug.security import check_password_hash
-from flask_login import login_user, logout_user
+from app.forms import LoginForm
 
-auth = Blueprint('auth', __name__, template_folder='templates')
+
+auth = Blueprint('auth', __name__, template_folder ='templates')
+
 
 # http://127.0.0.1:5000
 
-
-@auth.route('/login', methods=['GET'])
+@auth.route('/login', methods =['GET'])
 def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    form = LoginForm()
+    return render_template('auth/login.html', form=form)
 
-        user = User.query.filter_by(username=username).first()
+@auth.route('/login', methods = ["POST"])
+def login_post():
+    form = LoginForm()
 
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for("main.home"))
-        else:
-            flash("Invalid username or password")
+    if form.validate_on_submit():
+        flash('Not implemented', 'info')
+        return redirect(url_for('auth.login'))
 
-    return render_template("login.html")
+    flash('Invalid form data', 'danger')
+    return redirect(url_for('auth.login'))
+            
 
 
-@auth.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for("auth.login"))
