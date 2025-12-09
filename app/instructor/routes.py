9916060af1ce33c2e108fce4942effcabe8c83e5
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-12-08 18:48:40 -0800

    Updating instructor

--- app/instructor/routes.py
+++ app/instructor/routes.py
@@ -1,2 +1,23 @@
+from flask import render_template, Blueprint, redirect, url_for, flash, request
+from app.forms import AnnouncementForm
+from flask_login import current_user
+from app.models import Announcement
 
+instructor = Blueprint('instructor', __name__, template_folder='templates')
+
+# http://127.0.0.1:5000
+
+@instructor.route('/')
+def instructor_home():
+    return render_template('instructor/instructor_template.html', users=current_user)
+
+@instructor.route('/announcement/create', methods=['GET', 'POST'])
+def create_announcement():
+    form = AnnouncementForm()
+
+    if form.validate_on_submit():
+        announcement = Announcement(
+            title=form.title.data,
+            message=form.message.data,
+        )
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -9,8 +10,14 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(200))
     role = db.Column(db.String(20)) 
-class Course(db.model):
-    id = db.column(db.Integer, primary_key=True)
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
+
+class Announcement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     
