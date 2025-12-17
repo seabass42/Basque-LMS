c60473a93833daf4009d37166189a9dfe449106c
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-12-17 02:00:22 -0800

    Tests fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import pytest
-from app import create_app
+from app import create_app, db
 
 @pytest.fixture
 def app():
     app = create_app()
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['TESTING'] = True
-    return app
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
